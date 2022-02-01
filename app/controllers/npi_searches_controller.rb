65c9d0856555088e1ae005bb0d0ca0bc91f14456
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-31 21:35:16 -0500

    Add #new to NpiSearchesController

--- app/controllers/npi_searches_controller.rb
+++ app/controllers/npi_searches_controller.rb
@@ -1,2 +1,20 @@
+ENDPOINT_URL = "https://npiregistry.cms.hhs.gov/api"
+TARGET_VERSION = "2.1"
+
 class NpiSearchesController < ApplicationController
+  def new
+    @results = []
+    return unless params[:first_name].present? || params[:last_name].present?
+
+    query_string = {
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      version: TARGET_VERSION,
+      address_purpose: ""
+    }.to_query
+
+    uri = URI("#{ENDPOINT_URL}/?#{query_string}")
+    response = Net::HTTP.get_response(uri)
+    @results = JSON.parse(response.body)["results"]
+  end
 end
