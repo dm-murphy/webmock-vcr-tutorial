706342e2c6796532fd35b1eaffa1216821402d01
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-02-01 21:59:31 -0500

    Comment out webmock_test_spec.rb

--- spec/webmock_test_spec.rb
+++ spec/webmock_test_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'external HTTP request' do
-  it "can't hit Google" do
-    Net::HTTP.get("google.com", "/index.html")
-  end
-end
+# RSpec.describe 'external HTTP request' do
+#   it "can't hit Google" do
+#     Net::HTTP.get("google.com", "/index.html")
+#   end
+# end
